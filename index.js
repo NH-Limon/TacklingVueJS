f04d01fe7af00e093f01d0ad0b5146362c9e18f9
NH Limon <92261596+NH-Limon@users.noreply.github.com>
2021-12-22 15:11:47 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-const vm = new Vue({
-  el: "#app",
-  data: {
-    name: "Nahid Hasan Limon",
-    htmlContent: "<h2>This is a heading 2 tag</h2>",
-    boldText: "<b>This is a bold text</b>",
-    button: "<button>Click Me</button>",
-    image: "<img style = 'border-radius: 50%; ' src = 'apple.jpg'>",
-    imgsrc:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
-    textColor: "color: red;",
-    title:
-      "This is the title and the page is lastly refreshed on: " +
-      new Date().toLocaleString(),
-    text: "<h1>Hover On Me To See Something Interesting</h1>",
-  },
+const app = new Vue({
+    el: "#app",
+    data: {
+        name: 'Nahid Hasan Limon',
+        age: 20,
+        address: 'Barishal, Bangladesh' + " - Last Page Refreshed On : " + new Date().toLocaleTimeString()
+    }
 });
---
+++ index2.js
@@ -0,0 +1,21 @@
+// const app = new Vue({
+//     el: "#app",
+//     data: {
+//         seen: false,
+//         name : "Limon"
+//     }
+// })
+
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    things: [
+      { item: "Mango" },
+      { item: "Apple" },
+      { item: "Banana" },
+      { item: "Lichi" },
+      { item: "Pineapple" },
+    ],
+  },
+});
